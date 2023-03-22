efd34b5c8ecc7a5fcfd5030077096fcb1191a28e
marina-konkova <116936403+marina-konkova@users.noreply.github.com>
2023-03-22 14:00:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,41 +1,52 @@
 // Copyright 2021 NNTU-CS
-#include <algorithm>
 int countPairs1(int* arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = i + 1; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
-        count++;
-      }
-    }
-  }
-  return count;
+	int count = 0;
+	for (int k = 0; k < len-1; k++) {
+		for (int j = k+1; j < len; j++) {
+			if (arr[k] + arr[j] == value) {
+				count += 1;
+			}
+		}
+	}
+	return count;
 }
 int countPairs2(int* arr, int len, int value) {
-  int count = 0;
-  int i = 0, j = len - 1;
-  while (i < j) {
-    if (arr[i] + arr[j] == value) {
-      count++;
-      i++;
-      j--;
-    }
-    else if (arr[i] + arr[j] < value) {
-      i++;
-    }
-    else {
-      j--;
-    }
-  }
-  return count;
+	int count = 0;
+	for (int k = 0; k < len - 1; k++) {
+		for (int j = len - 1; k < j; j--) {
+			if (arr[k] + arr[j] == value) {
+				count += 1;
+			}
+		}
+	}
+	return count;
 }
 int countPairs3(int* arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; i++) {
-    int j = std::lower_bound(arr + i + 1, arr + len, value - arr[i]) - arr;
-    if (j < len && arr[j] == value - arr[i]) {
-      count++;
-    }
-  }
-  return count;
+	int count = 0;
+	for (int k = 0; k < len - 1; k++) {
+		int left = k, right = len;
+		while (left < right - 1) {
+			int middle = (left + right) / 2;
+			if (arr[k] + arr[middle] == value) {
+				count += 1;
+				int newmid = middle + 1;
+				while (arr[k] + arr[newmid] == value) {
+					count += 1;
+					newmid += 1;
+				}
+				newmid = middle - 1;
+				while (arr[k] + arr[newmid] == value) {
+					count += 1;
+					newmid -= 1;
+				}
+				break;
+			} 
+			if (arr[k] + arr[middle] > value) {
+				right = middle;
+			} else {
+				left = middle;
+			}
+		}
+	}
+	return count;
 }

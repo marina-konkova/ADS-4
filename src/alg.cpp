f42c3781c917430be680ef10e4125fc7e26cced2
marina-konkova <116936403+marina-konkova@users.noreply.github.com>
2023-03-22 13:39:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,9 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
+#include <algorithm>
+int countPairs1(int* arr, int len, int value)
+{
   int count = 0;
-  for (int i = 0; i < len-1; i++) {
+  for (int i = 0; i < len - 1; i++) {
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
         count++;
@@ -10,42 +12,32 @@ int countPairs1(int *arr, int len, int value) {
   }
   return count;
 }
-int countPairs2(int *arr, int len, int value) {
+int countPairs2(int* arr, int len, int value)
+{
   int count = 0;
-  for (int r = 0; r < len - 1; r++) {
-    for (int l = len - 1; r < l; l--) {
-      if (arr[r] + arr[l] == value) {
-        count += 1;
-      }
+  int i = 0, j = len - 1;
+  while (i < j) {
+    if (arr[i] + arr[j] == value) {
+      count++;
+      i++;
+      j--;
+    }
+    else if (arr[i] + arr[j] < value) {
+      i++;
+    }
+    else {
+      j--;
     }
   }
   return count;
 }
-int countPairs3(int *arr, int len, int value) {
+int countPairs3(int* arr, int len, int value)
+{
   int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    int left = i, right = len;
-    while (left < right - 1) {
-      int middle = (left + right) / 2;
-      if (arr[i] + arr[middle] == value) {
-        count += 1;
-        int newmid = middle + 1;
-        while (arr[i] + arr[newmid] == value) {
-          count += 1;
-          newmid += 1;
-        }
-        newmid = middle - 1;
-        while (arr[i] + arr[newmid] == value) {
-          count += 1;
-          newmid -= 1;
-        }
-        break;
-      }
-      if (arr[i] + arr[middle] > value) {
-        right = middle;
-      } else {
-        left = middle;
-      }
+  for (int i = 0; i < len; i++) {
+    int j = lower_bound(arr + i + 1, arr + len, value - arr[i]) - arr;
+    if (j < len && arr[j] == value - arr[i]) {
+      count++;
     }
   }
   return count;

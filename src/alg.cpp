9eb38b372d2eeabec2b66db13cbcecba5ea9d514
marina-konkova <116936403+marina-konkova@users.noreply.github.com>
2023-03-22 13:52:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
 // Copyright 2021 NNTU-CS
 #include <algorithm>
-int countPairs1(int* arr, int len, int value)
-{
+int countPairs1(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = i + 1; j < len; j++) {
@@ -12,8 +11,7 @@ int countPairs1(int* arr, int len, int value)
   }
   return count;
 }
-int countPairs2(int* arr, int len, int value)
-{
+int countPairs2(int* arr, int len, int value) {
   int count = 0;
   int i = 0, j = len - 1;
   while (i < j) {
@@ -31,11 +29,10 @@ int countPairs2(int* arr, int len, int value)
   }
   return count;
 }
-int countPairs3(int* arr, int len, int value)
-{
+int countPairs3(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; i++) {
-    int j = lower_bound(arr + i + 1, arr + len, value - arr[i]) - arr;
+    int j = std::lower_bound(arr + i + 1, arr + len, value - arr[i]) - arr;
     if (j < len && arr[j] == value - arr[i]) {
       count++;
     }
